0fcd8b386f7ea2224537e13e1ec9d2f1cc7ff403
Misael <misael@enviroblind.com>
2024-11-18 14:29:19 -0800

    misael:Updated Table-wall

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,17 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config"
+
+import tailwind from "@astrojs/tailwind"
+
+import db from "@astrojs/db"
+import icon from "astro-icon"
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    tailwind({
+      applyBaseStyles: false
+    }),
+    icon()
+  ]
+})
